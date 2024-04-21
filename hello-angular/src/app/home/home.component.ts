59aa938541cd6d32c0f3f3bb1715119839fd16a1
burak <burakb199726@gmail.com>
2024-04-21 11:42:28 +0300

    fourth lesson changes

--- hello-angular/src/app/home/home.component.ts
+++ hello-angular/src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import {CommonModule} from "@angular/common";
-import {HousingLocationComponent} from "../housing-location/housing-location.component";
+import { CommonModule } from "@angular/common";
+import { HousingLocationComponent } from "../housing-location/housing-location.component";
+import { HousingLocation } from "../housinglocation";
 
 @Component({
   selector: 'app-home',
@@ -23,5 +24,17 @@ import {HousingLocationComponent} from "../housing-location/housing-location.com
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
+
+  housingLocation: HousingLocation = {
+    id: 999,
+    name: 'Test Home',
+    city: 'Test City',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false
+  }
 
 }
