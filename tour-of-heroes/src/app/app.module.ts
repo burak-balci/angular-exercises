cae0dc25786651fe07c0aaea3c7d92a382ef179d
burak <burakb199726@gmail.com>
2024-04-21 19:53:13 +0300

    hero editor changes

--- tour-of-heroes/src/app/app.module.ts
+++ tour-of-heroes/src/app/app.module.ts
@@ -1,20 +1,21 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HeroesComponent} from "./heroes/heroes.component";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
-  providers: [
-    provideClientHydration()
-  ],
-  bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
